bca706e176a50e9fb020f50286941fe455bff40f
Prafful <Prafful@Platinum.com>
2018-03-13 02:18:06 +0530

    new chages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LandingComponentComponent } from './landing-component/landing-component.component'
-import { FilterdataComponent} from './filterdata/filterdata.component'
-import { SearchPipe } from'./filterdata/search.pipe'
+import { FilterdataComponent} from './filterdata/filterdata.component';
+import { SearchFilterPipePipe } from './filterdata/search-filter-pipe.pipe'
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchPipe,
     LandingComponentComponent,
-    FilterdataComponent
+    FilterdataComponent,
+    SearchFilterPipePipe
   ],
   imports: [
     BrowserModule
--- src/app/filterdata/filterdata.component.ts
+++ src/app/filterdata/filterdata.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SearchPipe} from './search.pipe'
+
 
 @Component({
   selector: 'app-filterdata',
